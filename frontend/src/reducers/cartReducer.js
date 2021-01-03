30783316e8ce76d7be9677b80d8077b59d30445f
Adepoju Lateef <tundeadepoju22@gmail.com>
2021-01-03 11:59:02 +0100

    added romove from cart functionality

--- frontend/src/reducers/cartReducer.js
+++ frontend/src/reducers/cartReducer.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM } from "../constants/CartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/CartConstants";
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
     switch (action.type) {
@@ -15,6 +15,12 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
                     ...state,
                     cartItems: [...state.cartItems, item]
                 };
+            }
+
+        case CART_REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(x => x.product !== action.payload)
             }
     
         default:
